85aa252ebc465e9c1e5e4766b82837e423f9a835
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-02-24 12:08:45 +0100

    To-dos can now be passe from AddToDo component to main state of the App

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ class App extends React.Component {
   state = {
     toDos: []
   }
+  handleAddToDo = (toDo) => {
+    if (!toDo) {
+      return 'Enter valid value to add a to-do';
+    } else if (this.state.toDos.indexOf(toDo) > -1) {
+      return 'This to-do already exists';
+    }
+
+    this.setState((prevState) => ({
+      toDos: prevState.toDos.concat(toDo)
+    }));
+  };
   render() {
     return (
       <div>
         <Header />
-        <AddToDo />
+        <AddToDo handleAddToDo={this.handleAddToDo} />
         <ToDos />
       </div>
     )
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -5,10 +5,21 @@ class AddToDo extends React.Component {
     state = {
         error: ''
     }
+    handleAddToDo = (e) =>{
+        e.preventDefault();
+        const toDo = e.target.elements.newToDo.value.trim();
+        const error = this.props.handleAddToDo(toDo);
+
+        this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.newToDo.value = '';
+    }
+    };
     render() {
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleAddToDo}>
                     <input type="text" name="newToDo" />
                     <button>Add To-Do</button>
                 </form>
