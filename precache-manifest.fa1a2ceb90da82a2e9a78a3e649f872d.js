3e89aec08ba918d5905ddca9bb440c48cdbc7ae4
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-02-26 13:11:26 +0100

    Updates

---
+++ precache-manifest.fa1a2ceb90da82a2e9a78a3e649f872d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b26c70ed72d8c98afede0610729257",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "9474dfa934d4bca1868a",
+    "url": "/to-do-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "558397c2706f145c13bb",
+    "url": "/to-do-app/static/css/main.1552581f.chunk.css"
+  },
+  {
+    "revision": "9474dfa934d4bca1868a",
+    "url": "/to-do-app/static/js/2.064141c1.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/to-do-app/static/js/2.064141c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558397c2706f145c13bb",
+    "url": "/to-do-app/static/js/main.c46a87eb.chunk.js"
+  },
+  {
+    "revision": "360964cdfa72339eab62",
+    "url": "/to-do-app/static/js/runtime-main.e18a26d3.js"
+  }
+]);
